fbc85506e9ba427bc74790a37476c83fa31b64c6
Your Other Name <your-email-account2@example.com>
2024-03-31 09:39:03 +0100

    empty funds page

--- app/(dashboard)/daos/view/[slug]/funds/page.tsx
+++ app/(dashboard)/daos/view/[slug]/funds/page.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 const EachDaoFunds = () => {
     return (
-        <div>Funds</div>
+        <div>
+            <div className="text-center w-[30%] space-y-4 mx-auto mt-[10%]">
+                <p className="text-sm font-light text-defaultText">Currently, there are no funds in the treasury. You can initiate a proposal to make deposit.</p>
+                <Button>Deposit Token</Button>
+            </div>
+        </div>
     )
 };
 
