52254618a8c57012ee4b3044630d49f276abb74e
Your Other Name <your-email-account2@example.com>
2024-04-01 15:35:23 +0100

    refactor settings layout

--- app/(dashboard)/settings/layout.tsx
+++ app/(dashboard)/settings/layout.tsx
@@ -1,36 +1,9 @@
 'use client';
-import { Bell2, DisplayIcon, OnePersonicon } from '@/assets/svgs';
-import {
-  SETTINGS_DISPLAY_URL,
-  SETTINGS_NOTIFICATIONS_URL,
-  SETTINGS_URL,
-} from '@/config/path';
 import { cn } from '@/libs/utils';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { ReactNode } from 'react';
-
-export const daoSettingsSidebarLinks: {
-  title: string;
-  href: string;
-  icon: ReactNode;
-}[] = [
-  {
-    title: 'My Profile',
-    icon: <OnePersonicon />,
-    href: SETTINGS_URL,
-  },
-  {
-    title: 'Notifications',
-    icon: <Bell2 width="23" height="26" />,
-    href: SETTINGS_NOTIFICATIONS_URL,
-  },
-  {
-    title: 'Display',
-    icon: <DisplayIcon />,
-    href: SETTINGS_DISPLAY_URL,
-  },
-];
+import { settingsSidebarLinks } from './config';
 
 interface ILayout {
   children: ReactNode;
@@ -45,7 +18,7 @@ const Layout = ({ children }: ILayout) => {
       <div className="flex justify-between items-start space-x-4">
         <div className="w-[20%] bg-[#191919] rounded-lg p-4">
           <div className="space-y-6 text-[#888888] text-sm">
-            {daoSettingsSidebarLinks.map((link) => (
+            {settingsSidebarLinks.map((link) => (
               <Link key={link.title} href={link.href}>
                 <div
                   className={cn(
