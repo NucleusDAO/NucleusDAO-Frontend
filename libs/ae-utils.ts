8b7eb2c3715f5de768348f0bde743f806c299c18
Hexdee <lukmansaheed79@gmail.com>
2024-05-19 12:18:13 +0100

    Update contract address and ACIs

--- libs/ae-utils.ts
+++ libs/ae-utils.ts
@@ -11,8 +11,7 @@ import nucleusDAOAci from './contract/NucleusDAO.json';
 import basicDAOAci from './contract/BasicDAO.json';
 import { DASHBOARD_URL } from '@/config/path';
 
-const nucleusDAOContractAddress =
-  'ct_FVFpGujmmLv4UEuJczdTMG3RekDt5jJBe32H9e2PHPMn2UrBb';
+const nucleusDAOContractAddress = 'ct_DPR9rN4U7drVvzm83yhCBr6wz9vuw4y54mDcKvEp9Z13kecqE';
 
 const TESTNET_NODE_URL = 'https://testnet.aeternity.io';
 const MAINNET_NODE_URL = 'https://mainnet.aeternity.io';
